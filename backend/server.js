18d0093131afb79c666529886ea10812a1d6ced3
Bhuwan-Upadhyay <bhuwanb898@gmail.com>
2025-11-10 19:09:51 +0530

    feat:sign-up and auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import { PORT } from "./config/config.js"
 import connectToDatabase from './database/mongodb.js';
+import cookieParser from 'cookie-parser';
+import authRouter from './routes/auth.routes.js';
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/v1/auth', authRouter);
 
 app.get("/", (req, res) => {
     res.json({ message: "instaNeeds" });
