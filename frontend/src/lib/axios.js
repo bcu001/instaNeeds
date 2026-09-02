79dcf4765889dd021edfaa66c31629bacf66630e
Bhuwan Upadhyay <bhuwan.upadhyay.work@gmail.com>
2026-09-02 13:17:36 +0530

    chore: added axios interceptors for auth token insertion

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,17 @@
 import axios from "axios";
-import { server_url } from "./env";
+import { server_url, token } from "./env";
 
 
 const api = axios.create({
     baseURL: `${server_url}`
+})
+
+api.interceptors.request.use((config)=>{
+    const _token = token;
+    if(token){
+        config.headers.Authorization = `Bearer ${_token}`;
+    }
+    return config;
 })
 
 export default api;
